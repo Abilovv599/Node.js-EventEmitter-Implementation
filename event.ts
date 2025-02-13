6baeacfbe3f9ea23415fb2a416e6ed23295c6f36
abilovv599 <abilovv599@gmail.com>
2025-02-13 13:33:44 +0400

    initial commit

---
+++ event.ts
@@ -0,0 +1,27 @@
+type Callback = (...args: any[]) => any;
+type Subscription = {
+  unsubscribe: () => void;
+};
+
+class EventEmitter {
+  private events = new Map();
+
+  subscribe(eventName: string, callback: Callback): Subscription {
+    if (!this.events.has(eventName)) {
+      this.events.set(eventName, []);
+    }
+    this.events.get(eventName).push(callback);
+    return {
+      unsubscribe: () => {
+        this.events.set(
+          eventName,
+          this.events.get(eventName).filter((cb: any) => cb !== callback)
+        );
+      },
+    };
+  }
+
+  emit(eventName: string, args: any[] = []): any[] {
+    return (this.events.get(eventName) ?? []).map((cb: any) => cb(...args));
+  }
+}
